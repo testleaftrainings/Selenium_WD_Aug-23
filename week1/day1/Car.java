021a8c9ea018ad52ea0672840e93177c9a6548c9
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-08-09 14:51:08 +0530

    Add files via upload

---
+++ week1/day1/Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+	     //main method-->single comment line and not execute
+	   /*
+	    * step1
+	    * step2
+	    */  //--->multi comment line
+	public static void main(String[] args) {
+		//simple print statement
+		System.out.println("%%Welcome To Testleaf 123 $$$");
+	
+	
+	}
+}
---
+++ week1/day1/LearnObject.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+//Class
+public class LearnObject {
+
+	//main method
+	public static void main(String[] args) {
+		//Creating object for class
+		LearnObject  obj = new LearnObject();
+		
+		
+
+	}
+
+}
